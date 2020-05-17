47df9fc03bb75c3e167ec73dd3513574ec671f3f
Knight <tocaldera6734@gmail.com>
2020-05-17 14:09:28 +0530

    added resampling

---
+++ resampling.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df = pd.read_csv('BA.csv',parse_dates=['Date'],index_col=['Date'])
+#converting only to the monthly data
+print(df.Close.resample('M').mean())
+
+#%%
+import pandas as pd
+import matplotlib as mp
+
+df = pd.read_csv('BA.csv',parse_dates=['Date'],index_col=['Date'])
+#converting only to the monthly data
+
+df.Close.resample('W').mean().plot()
+
+
+# %%
+#in here kind is bar
+df.Close.resample('M').mean().plot(kind="bar")
+
+# %%
