834cbbb3bb176f620c8d1c697ceac18b05509913
Knight <tocaldera6734@gmail.com>
2020-05-24 21:16:46 +0530

    adfuller completed

--- AugmentedDickeyFullerTest.py
+++ AugmentedDickeyFullerTest.py
@@ -1,3 +1,11 @@
+'''
+p-value > 0.05: Fail to reject the null hypothesis (H0), the data has a unit root and is non-stationary.
+p-value <= 0.05: Reject the null hypothesis (H0), the data does not have a unit root and is stationary.
+
+ADF Statistic
+if 1% is less than ADF Statistic value. Then that is non-stationary
+
+'''
 from pandas import read_csv
 from statsmodels.tsa.stattools import adfuller
 
@@ -5,6 +13,11 @@ series = read_csv('sample1.csv', header=0, index_col=0, squeeze=True)
 X = series.values
 result = adfuller(X)
 print('ADF Statistic: %f' % result[0])
+# Uncomment for easy use :) 
+# if result[1]>0.05:
+# 	print("non-stationary")
+# else:
+# 	print("Stationry")
 print('p-value: %f' % result[1])
 print('Critical Values:')
 for key, value in result[4].items():
