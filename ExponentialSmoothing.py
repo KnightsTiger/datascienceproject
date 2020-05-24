241fac482ed6077d6353e79f46e3b4030a61cc76
Knight <tocaldera6734@gmail.com>
2020-05-24 11:16:09 +0530

    ExponentialSmoothing

--- ExponentialSmoothing.py
+++ ExponentialSmoothing.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot  as plt
 
 df = pd.read_csv('sample1.csv')
 
-#apply Simple Exponential Smoothing 
+#apply Simple Exponential Smoothing
 
 # fit3 = SimpleExpSmoothing(df).fit()
 # fcast3 = fit3.forecast(12).rename(r'$\alpha=%s$'%fit3.model.params['smoothing_level'])
@@ -24,5 +24,28 @@ df = pd.read_csv('sample1.csv')
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
-import statsmodels.tsa.holtwinters as sts
+import statsmodels.tsa.holtwinters as ets
 
+df = pd.read_csv('sample1.csv',index_col='Date',parse_dates=True)
+#print(df)
+#Defining the range of the training and testing
+spyt = df[:'2020-01-10']
+spyf= df['2020-01-11':]
+
+#print(spyt)
+#print(spyf)
+
+brownt = ets.ExponentialSmoothing(spyt,trend=None,damped=False,seasonal=None).fit()
+brownf = brownt.forecast(steps=len(spyf))
+brownf = pd.DataFrame(brownf).set_index(spyf.index)
+
+fig1, ax = plt.subplots()
+ax.plot(spyt,label='spyt')
+ax.plot(spyf,label='spyf')
+ax.plot(brownf,label='brownf')
+plt.ylabel('likes')
+plt.xlabel('hours')
+plt.show()
+
+
+# %%
