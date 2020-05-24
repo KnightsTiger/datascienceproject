b6747de51aefe4e289134d3cbd4ff2bda46fc9ff
Knight <tocaldera6734@gmail.com>
2020-05-24 10:17:46 +0530

    updated code

--- ExponentialSmoothing.py
+++ ExponentialSmoothing.py
@@ -1,3 +1,28 @@
+#%%
 # 1) install statsmodels
 # pip install statsmodels
 from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
+import pandas as pd
+import matplotlib.pyplot  as plt
+
+df = pd.read_csv('sample1.csv')
+
+#apply Simple Exponential Smoothing 
+
+# fit3 = SimpleExpSmoothing(df).fit()
+# fcast3 = fit3.forecast(12).rename(r'$\alpha=%s$'%fit3.model.params['smoothing_level'])
+# # plot
+# fcast3.plot(marker='o', color='green', legend=True)
+# fit3.fittedvalues.plot(marker='o', color='green')
+
+# plt.show()
+
+
+
+
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.tsa.holtwinters as sts
+
